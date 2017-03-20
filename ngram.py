6eaec7f0e34472e7d8790715f6a4beeed7a052cd
chenyangha <hchenyang@hotmail.com>
2017-03-19 20:36:59 -0600

    nothing

--- ngram.py
+++ ngram.py
@@ -1,16 +1,18 @@
 from os import listdir
 from os.path import isfile, join
-
+import string
 from nltk.util import ngrams
-
 
 def read_files(dir_path):
     onlyfiles = [f for f in listdir(dir_path) if isfile(join(dir_path, f))]
     data = {}
     for file in onlyfiles:
-        lang = file.split('.')[0]
+        lang = file.split('.')[0].split('-')[1]
         with open(join(dir_path, file)) as f:
-            data[lang] = f.read()
+            text = f.read()
+            # Remove  punctuation TODO: remove Space?
+            data[lang] = ' '.join(filter(None,
+                                        (word.strip(string.punctuation) for word in text.split())))
     return data
 
 
@@ -27,7 +29,9 @@ def __main__():
     dev_path = '650_a3_dev'
     train_data = read_files(train_path)
     dev_data = read_files(dev_path)
-    pasue = 0
+    test_data = train_data['als']
+    generated_ngrams = ngrams(test_data, 4, pad_left=True, pad_right=True)
+
 
 
 __main__()
